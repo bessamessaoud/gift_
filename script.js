f139b08649693840319b5e5f91a31cdc5f0065c1
BESSA <messaoudsetze@gmail.com>
2024-01-03 09:38:17 +0100

    first commit....

--- script.js
+++ script.js
@@ -304,7 +304,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "http://www.youtube.com/embed/uzAWDG3ZieM?si=clCEiCUS85Q6upr2");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/uzAWDG3ZieM?si=clCEiCUS85Q6upr2");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   //ifrm.allow = "autoplay"
